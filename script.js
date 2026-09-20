d3c245bc1abd2ae58b8c0d39c3a5ec20e104e41a
naressifreiremichel-lang <naressifreiremichel@gmail.com>
2026-09-19 22:10:40 -0300

    Update script.js
    adicionar busca

--- script.js
+++ script.js
@@ -23,21 +23,44 @@ async function carregarPerfil() {
   document.getElementById('status').textContent =
     status;
 }
-async function carregarSteam() {
+async function carregarSteam()}
+
+renderizar(jogos);
+
+pesquisa.addEventListener('input', () => {
+
+  const texto =
+    pesquisa.value.toLowerCase();
+
+  const filtrados =
+    jogos.filter(jogo =>
+      jogo.name.toLowerCase().includes(texto)
+    );
+
+  renderizar(filtrados);
+
+}); {
   const resposta = await fetch('./data/steam.json');
   const dados = await resposta.json();
 
   const jogos = dados.response.games || [];
+  const pesquisa =
+  document.getElementById('pesquisa');
 document.getElementById('totalJogos').textContent =
   jogos.length + ' jogos';
   const container = document.getElementById('games');
 
   container.innerHTML = '';
 
-  jogos
-    .sort((a, b) => b.playtime_forever - a.playtime_forever)
+  function renderizar(lista) {
+
+  container.innerHTML = '';
+
+  lista
+    .sort((a, b) =>
+      b.playtime_forever - a.playtime_forever
+    )
     .forEach(jogo => {
-      const horas = Math.round(jogo.playtime_forever / 60);
 
       const card = document.createElement('div');
       card.className = 'card';
