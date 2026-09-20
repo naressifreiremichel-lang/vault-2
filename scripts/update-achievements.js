c850b18477d26315ee9eb519bce13dd7e741f80e
naressifreiremichel-lang <naressifreiremichel@gmail.com>
2026-09-20 13:51:34 -0300

    Update update-achievements.js
    sistema de conquistas

--- scripts/update-achievements.js
+++ scripts/update-achievements.js
@@ -1,1 +1,45 @@
-console.log("Gerando conquistas...");
+const fs = require('fs');
+
+try {
+
+  const steam =
+    JSON.parse(
+      fs.readFileSync(
+        'data/steam.json',
+        'utf8'
+      )
+    );
+
+  const jogos =
+    steam.response.games || [];
+
+  const conquistas = {};
+
+  jogos.forEach(jogo => {
+
+    conquistas[jogo.appid] = {
+      nome: jogo.name,
+      desbloqueadas: 0,
+      total: 0
+    };
+
+  });
+
+  fs.writeFileSync(
+    'data/conquistas.json',
+    JSON.stringify(
+      conquistas,
+      null,
+      2
+    )
+  );
+
+  console.log(
+    'conquistas.json gerado'
+  );
+
+} catch (erro) {
+
+  console.error(erro);
+
+}
