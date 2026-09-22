69dcd5c229bb01afbece04b8aa0a10d26ebc4f93
naressifreiremichel-lang <naressifreiremichel@gmail.com>
2026-09-21 21:51:38 -0300

    Create update-friend-achievements.js

---
+++ scripts/update-friend-achievements.js
@@ -0,0 +1,91 @@
+const fs = require('fs');
+
+async function main() {
+
+  const apiKey =
+    process.env.STEAM_API_KEY;
+
+  const jogosPorAmigo =
+    JSON.parse(
+      fs.readFileSync(
+        'data/friend-games.json',
+        'utf8'
+      )
+    );
+
+  const resultado = {};
+
+  for (const steamid of Object.keys(jogosPorAmigo)) {
+
+    resultado[steamid] = {};
+
+    const jogos =
+      jogosPorAmigo[steamid];
+
+    for (const jogo of jogos) {
+
+      try {
+
+        const resposta =
+          await fetch(
+            `https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v1/?key=${apiKey}&steamid=${steamid}&appid=${jogo.appid}`
+          );
+
+        const dados =
+          await resposta.json();
+
+        const conquistas =
+          dados.playerstats
+            ?.achievements || [];
+
+        const desbloqueadas =
+          conquistas.filter(
+            a => a.achieved === 1
+          ).length;
+
+        const total =
+          conquistas.length;
+
+        const percentual =
+          total > 0
+            ? Math.round(
+                desbloqueadas /
+                total *
+                100
+              )
+            : 0;
+
+        resultado[steamid][jogo.appid] = {
+
+          nome: jogo.name,
+
+          desbloqueadas,
+
+          total,
+
+          percentual
+
+        };
+
+      } catch {}
+
+    }
+
+  }
+
+  fs.writeFileSync(
+    'data/friend-achievements.json',
+    JSON.stringify(
+      resultado,
+      null,
+      2
+    )
+  );
+
+  console.log(
+    'friend-achievements.json gerado'
+  );
+
+}
+
+main();
