ebccb57a95d71831d9e345565bee5d9fc415046c
naressifreiremichel-lang <naressifreiremichel@gmail.com>
2026-09-23 19:50:04 -0300

    Update amigo.js

--- amigo.js
+++ amigo.js
@@ -55,28 +55,37 @@ async function carregarAmigo() {
   ).textContent =
     amigo.steamid;
 
-  document.getElementById(
-    'musicaAmigo'
-  ).innerHTML = `
+  if (
+    amigo.nome &&
+    amigo.nome.toLowerCase().includes(
+      'ovos'
+    )
+  ) {
 
-    <audio
-      controls
-      autoplay
-      loop
-      style="
-        width:100%;
-        margin-top:20px;
-      "
-    >
+    document.getElementById(
+      'musicaAmigo'
+    ).innerHTML = `
 
-      <source
-        src="./audio/030bc8fd-15b4-4c68-a809-90c1d24e97d5%20(1)%20(mp3cut.net)%20(3).mp3"
-        type="audio/mpeg"
+      <audio
+        controls
+        autoplay
+        loop
+        style="
+          width:100%;
+          margin-top:20px;
+        "
       >
 
-    </audio>
+        <source
+          src="./audio/030bc8fd-15b4-4c68-a809-90c1d24e97d5%20(1)%20(mp3cut.net)%20(3).mp3"
+          type="audio/mpeg"
+        >
 
-  `;
+      </audio>
+
+    `;
+
+  }
 
   const respostaJogos =
     await fetch(
